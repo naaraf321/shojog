a7c21121f22b2aa3f7c30e2720b755587b33c034
N A Araf <83575957+naaraf321@users.noreply.github.com>
2025-03-27 23:53:01 +0600

    Update SubjectSelectionInterface.tsx

--- src/components/question-bank/SubjectSelectionInterface.tsx
+++ src/components/question-bank/SubjectSelectionInterface.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useState } from "react";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Input } from "@/components/ui/input";
