9c8fb7e787ab8e7303f21ab9139e001400fc7745
N A Araf <83575957+naaraf321@users.noreply.github.com>
2025-03-27 23:49:32 +0600

    Update ChapterTopicFiltering.tsx

--- src/components/question-bank/ChapterTopicFiltering.tsx
+++ src/components/question-bank/ChapterTopicFiltering.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState } from "react";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
